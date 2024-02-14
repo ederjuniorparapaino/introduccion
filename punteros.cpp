ad749799c9d6d78bf5f08f892c4e4ee729434057
ederjuniorparapaino <159298694+ederjuniorparapaino@users.noreply.github.com>
2024-02-14 14:22:36 -0400

    Add files via upload

---
+++ punteros.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int v[]={6,7,8};
+	int *pi=NULL;
+	int num;
+	num=v[0];
+	pi=v; //puntando al primer elemento.
+	cout<<"num:"<<num<<endl;
+	cout<<"pi:"<<*pi<<endl;	
+	
+	for (int i = 0; i < 3; ++i) {
+		num = v[i];
+		cout << "num: " << num << endl;
+		cout << "pi: " << *pi << endl;
+		pi++;
+	}
+	return 0;
+}
