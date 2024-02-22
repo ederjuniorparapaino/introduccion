8c82c9c3e08f0809aabd6984ea3eb0e61d6941d1
ederjuniorparapaino <159298694+ederjuniorparapaino@users.noreply.github.com>
2024-02-22 14:24:13 -0400

    Add files via upload

---
+++ funcion recursiva.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimir(int *n, int cant)
+{
+	for(int i=0;i<cant;++i)
+		cout<<i<<"-"<<n[i]<<endl;
+}
+void imprec(int *n, int cant)
+{
+	if(cant>0)
+	{
+		imprec(n,cant-1);
+		cout<<cant<<"-"<<n[cant-1];
+	}
+}
+// FUNCION BUSQUEDA
+
+
+// FUNCION MAYOR
+void mayor(int *n, int cant, int &m)
+{
+	if(cant>0)
+	{
+		if(n[cant-1]>m)
+			m=n[cant=1];
+		mayor(n,cant-1,m);
+		
+	}
+}
+int main(int argc, char *argv[]) {
+	const int TAM=7;
+	int m=0;
+	int numeros[]={3,5,2,1,7,4};
+	cout<<"IMPRESION BUCLE"<<endl;
+	imprimir(numeros, TAM);
+	cout<<"IMPRESION RECURSIVA"<<endl;
+	imprec(numeros,TAM);
+	mayor(numeros,TAM,m);
+	cout<<"MAYOR ES:"<<m<<endl;
+	
+	
+	return 0;
+}
+
