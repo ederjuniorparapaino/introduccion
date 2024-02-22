23d4f1bebc7e452f74ce2aba6b411bf5dfda7f44
ederjuniorparapaino <159298694+ederjuniorparapaino@users.noreply.github.com>
2024-02-22 14:23:48 -0400

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fib(int n){
+	if(n <= 1)
+		return n; //condicion base
+	else
+		{
+		//cout<<(n-1)<<", "<<(n-2)<<endl;
+		return fib(n-1)+fib(n-2); //condicion recursiva
+	}
+}
+int main(int argc, char *argv[]) {
+	const int NUM=5;
+	cout<<"Fibonacci de:"<<NUM<<"-"<<fib(NUM)<<endl;
+	return 0;
+}
+
